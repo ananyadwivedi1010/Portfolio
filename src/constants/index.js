a1fc4d1da23537fdcfa5e1f7dcdf033a7a76abba
Ananya Dwivedi <ananya10dwivedi@gmail.com>
2026-01-25 19:13:56 +0530

    fixed test

--- src/constants/index.js
+++ src/constants/index.js
@@ -135,8 +135,6 @@ const experiences = [
 ];
 
 
-const testimonials = [];
-
 
 const projects = [
   {
@@ -233,28 +231,6 @@ const projects = [
     image: localmark,
     source_code_link: "https://github.com/ananyadwivedi1010/LocalMark",
   },
-
-  // {
-  //   name: "Trip Guide",
-  //   description:
-  //     "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-  //   tags: [
-  //     {
-  //       name: "nextjs",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "supabase",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "css",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: tripguide,
-  //   source_code_link: "https://github.com/",
-  // },
 ];
 
-export { services, technologies, experiences, testimonials, projects };
+export { services, technologies, experiences, projects };
