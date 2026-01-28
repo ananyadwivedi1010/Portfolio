21a50c6cb6699c8dd52eabaab7a18eebaf9ac322
Ananya Dwivedi <ananya10dwivedi@gmail.com>
2026-01-28 22:30:13 +0530

    fixe

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,7 +7,7 @@ const ComputersCanvas = () => {
       <img
         src={profile}
         alt='profile'
-        className='w-80 h-80 lg:w-[450px] lg:h-[450px] rounded-full object-cover border-4 border-secondary mt-28 lg:mt-48'
+        className='w-64 h-64 lg:w-[400px] lg:h-[400px] rounded-full object-cover border-4 border-secondary mt-28 lg:mt-48'
       />
     </div>
   );
